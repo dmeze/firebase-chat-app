ba6dac7b8025d88edba289ad1f237f3c831706a1
Danylo Mezentsev <dmezents@cisco.com>
2025-02-04 23:37:03 +0100

    dmezents: Always scroll down on new message

--- src/components/messages/MessageList.jsx
+++ src/components/messages/MessageList.jsx
@@ -1,11 +1,21 @@
+import { useEffect, useRef } from "react";
 import MessageItem from "./MessageListItem.jsx";
 
-const MessageList = ({ messages, users }) => (
-    <div className="h-96 overflow-y-scroll border border-gray-300 p-4 rounded mb-4">
-        {messages.map((message) => (
-            <MessageItem key={message.id} message={message} users={users} />
-        ))}
-    </div>
-);
+const MessageList = ({ messages, users }) => {
+    const messagesEndRef = useRef(null);
+
+    useEffect(() => {
+        messagesEndRef.current?.scrollIntoView({behavior: "smooth"});
+    }, [messages]);
+
+    return (
+        <div className="h-96 overflow-y-scroll border border-gray-300 p-4 rounded mb-4">
+            {messages.map((message) => (
+                <MessageItem key={message.id} message={message} users={users}/>
+            ))}
+            <div ref={messagesEndRef} />
+        </div>
+    );
+}
 
 export default MessageList;
